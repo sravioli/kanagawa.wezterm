9e7ef75c4ccd22ff523589c93e2776bcc0209b72
sravioli <sravioli+github@proton.me>
2024-08-01 12:12:51 +0200

    test: check if require is successfull

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,9 +1,21 @@
 local wt = require "wezterm"
 
+local plugin = wt.plugin.list()[1]
+if wt.target_triple:find "windows" ~= nil then
+  local plugin_dir = plugin.plugin_dir:gsub("\\[^\\]*$", "")
+  package.path = package.path .. ";" .. plugin_dir .. "\\?."
+else
+  local plugin_dir = plugin.plugin_dir:gsub("/[^/]*$", "")
+  package.path = package.path .. ";" .. plugin_dir .. "/?."
+end
+
+local function lquire(module)
+  return require(plugin.component .. "." .. module)
+end
+
 local M = {}
 
-local path = ...
-wt.log_info { path = path }
+local wave = lquire "plugin.schemes.kanagawa-wave"
 
 M.apply_to_config = function(Config)
   return Config
